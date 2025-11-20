976da2fd3e3b007348504e409838f6d49e5e9d7f
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-11-20 22:34:26 +0530

    deployin on keyeb

---
+++ qrproject/qrapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import qrcode
+import os
+from django.conf import settings
+
+def index(request):
+    return render(request,'index.html')
+
+def generate_qr(request):
+    if request.method == 'POST':
+        url = request.POST.get('url')
+        if not url:
+            return JsonResponse({'error': 'Please provide a URL'})
+        qr = qrcode.make(url)
+        qr_path = os.path.join(settings.BASE_DIR, 'static', 'qr.png')
+        qr.save(qr_path)
+        qr_url = os.path.join(settings.STATIC_URL, 'qr.png')
+        return render(request, 'index.html',  {'qr_url': qr_url})
+
+    return render(request, 'index.html')
