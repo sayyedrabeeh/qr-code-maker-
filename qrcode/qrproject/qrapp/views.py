9df75ec1f6e65c3747399af1ec483bf1efe31ebb
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-11-20 21:51:57 +0530

    removed prints

--- qrcode/qrproject/qrapp/views.py
+++ qrcode/qrproject/qrapp/views.py
@@ -7,22 +7,14 @@ from django.conf import settings
 def index(request):
     return render(request,'index.html')
 
-# 
-
 def generate_qr(request):
     if request.method == 'POST':
         url = request.POST.get('url')
-        print('url:',url)
         if not url:
             return JsonResponse({'error': 'Please provide a URL'})
-        
-    
         qr = qrcode.make(url)
         qr_path = os.path.join(settings.BASE_DIR, 'static', 'qr.png')
-        
-     
         qr.save(qr_path)
-        print('qr_url:', settings.STATIC_URL + 'qr.png') 
         qr_url = os.path.join(settings.STATIC_URL, 'qr.png')
         return render(request, 'index.html',  {'qr_url': qr_url})
 
